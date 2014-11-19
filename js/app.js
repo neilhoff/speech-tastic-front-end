fb9d0c558fbc8e7e090dc55adc56e9662d395cee
Neil Hoff <neilhoff@gmail.com>
2014-11-19 07:41:39 -0600

    Implement ng-route

--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
 (function() {
-    var app = angular.module('speech-tastic', []);
+    var app = angular.module('speech-tastic', ['ngRoute']);
+
+    // configure our routes
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider
+
+                .when('/', {
+                    templateUrl: 'templates/latest-posts.html',
+                    controller: 'LatestPostsController'
+                })
+
+                .when('/posts/:slug', {
+                    templateUrl: 'templates/show-post.html',
+                    controller: 'ShowPostController'
+                });
+        }]);
+
+
 
     // Controllers
-    app.controller('SiteController', ['$http', function($http) {
+    app.controller('LatestPostsController', ['$scope', '$http', function($scope, $http) {
+
 
     }]);
 
+    app.controller('ShowPostController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+        $scope.slug = $routeParams.slug;
+        console.log($scope.slug);
+
+    }]);
+
+    // Directives
     app.directive('topNav', function(){
         return {
             restrict: 'E',
             templateUrl: 'templates/top-nav.html'
-        };
-    });
-
-    app.directive('latestPosts', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/latest-posts.html'
         };
     });
 
