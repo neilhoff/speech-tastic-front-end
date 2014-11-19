dc51774b72333c9a9042fcecab0cf4a7a2efaca6
Neil Hoff <neilhoff@gmail.com>
2014-11-18 21:23:38 -0600

    Finish layout and styles of home page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 (function() {
-    var app = angular.module('speechTastic', []);
+    var app = angular.module('speech-tastic', []);
 
-});
+    // Controllers
+    app.controller('SiteController', ['$http', function($http) {
+
+    }]);
+
+    app.directive('topNav', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/top-nav.html'
+        };
+    });
+
+    app.directive('latestPosts', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/latest-posts.html'
+        };
+    });
+
+})();
